faea961c695813d42ab8733282fbbfc994edf12b
j-sungyeong <jang.s.g.125@gmail.com>
2021-04-15 16:42:08 +0900

    Fix: git conflict removed

---
+++ pulpissimo/fpga/pulpissimo-zcu102/rtl/pulpissimo_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/15/2021 03:52:21 PM
+// Design Name: 
+// Module Name: pulpissimo_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pulpissimo_tb(
+
+    );
+endmodule
